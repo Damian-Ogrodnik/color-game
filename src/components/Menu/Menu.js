a08a5a6e4b64b5c5b2606a0ad4c303d6bbd76add
dogrodnik <d.ogrodnik96@gmail.com>
2019-12-27 15:21:03 +0100

    Create menu layout

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,16 +6,18 @@ export const Menu = () => {
       <div className="menu__header">
         <h1>Color Game</h1>
       </div>
-      <div className="menu__info">?</div>
       <div className="menu__difficulty">
         <h2>Select level of difficulty</h2>
-        <button className="btn__level--easy ">Easy</button>
-        <button className="btn__level--medium ">Medium</button>
-        <button className="btn__level--hard ">Hard</button>
+        <div className="menu__levels">
+          <button className="btn__level--easy ">Easy</button>
+          <button className="btn__level--medium ">Medium</button>
+          <button className="btn__level--hard ">Hard</button>{" "}
+        </div>
       </div>
       <div className="menu__start">
         <button className="btn__start">Start</button>
       </div>
+      <div className="menu__info">?</div>
     </div>
   );
 };
