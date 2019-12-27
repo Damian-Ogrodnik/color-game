3cf6db470eb7fb2d41c7f51d927e0aa323e026df
dogrodnik <d.ogrodnik96@gmail.com>
2019-12-27 15:00:38 +0100

    Create basic menu html structure

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      <div className="menu__header">
+        <h1>Color Game</h1>
+      </div>
+      <div className="menu__info">?</div>
+      <div className="menu__difficulty">
+        <h2>Select level of difficulty</h2>
+        <button className="btn__level--easy ">Easy</button>
+        <button className="btn__level--medium ">Medium</button>
+        <button className="btn__level--hard ">Hard</button>
+      </div>
+      <div className="menu__start">
+        <button className="btn__start">Start</button>
+      </div>
+    </div>
+  );
 };
