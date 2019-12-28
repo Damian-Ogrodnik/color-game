01dd8b5a8e742517690e323fa1c6af8796b58b68
dogrodnik <d.ogrodnik96@gmail.com>
2019-12-28 10:46:54 +0100

    Basic menu style

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -7,17 +7,19 @@ export const Menu = () => {
   const { dispatch } = globalState;
   return (
     <div className="menu">
-      <div className="menu__header">
-        <h1>Color Game</h1>
-      </div>
-      <Levels />
-      <div className="menu__start">
-        <button
-          className="btn__start"
-          onClick={() => dispatch({ type: "START_GAME" })}
-        >
-          START
-        </button>
+      <div className="menu__wrapper">
+        <div className="menu__header">
+          <h1>Color Game</h1>
+        </div>
+        <Levels />
+        <div className="menu__start">
+          <button
+            className="btn__start"
+            onClick={() => dispatch({ type: "START_GAME" })}
+          >
+            START
+          </button>
+        </div>
       </div>
       <div className="menu__info">?</div>
     </div>
