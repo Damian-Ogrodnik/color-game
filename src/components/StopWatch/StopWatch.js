a5d7ade0a9e0ee2012940ad4955885f686352d9b
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-01 11:57:23 +0100

    Basic logic in StopWatch

--- src/components/StopWatch/StopWatch.js
+++ src/components/StopWatch/StopWatch.js
@@ -1,15 +1,54 @@
 import React, { useState, useEffect } from "react";
 
 export const StopWatch = () => {
+  const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const [miliSeconds, setMiliSeconds] = useState(0);
+  // const [tenthSeconds, setTenthSeconds] = useState(0);
+  // const [miliSeconds, setMiliSeconds] = useState(0);
+
   useEffect(() => {
     setTimeout(() => setSeconds(seconds + 1), 1000);
   });
 
-  const renderTime = () => {
-    return seconds;
+  const renderMinutes = () => {
+    if (minutes < 10) {
+      return `0${minutes}`;
+    } else return minutes;
   };
 
-  return <div>{renderTime()}</div>;
+  const renderSeconds = () => {
+    if (seconds < 10) {
+      return `0${seconds}`;
+    } else if (seconds > 59) {
+      setMinutes(minutes + 1);
+      setSeconds(0);
+      return "00";
+    } else {
+      return seconds;
+    }
+  };
+
+  // const partSeconds = () => {
+  //   if (tenthSeconds === 10) {
+  //     setSeconds(seconds + 1);
+  //     setTenthSeconds(0);
+  //   } else {
+  //     return tenthSeconds;
+  //   }
+  // };
+
+  // const renderMiliSeconds = () => {
+  //   if (miliSeconds === 10) {
+  //     setTenthSeconds(tenthSeconds + 1);
+  //     setMiliSeconds(0);
+  //   } else {
+  //     return miliSeconds;
+  //   }
+  // };
+
+  return (
+    <div>
+      {renderMinutes()}:{renderSeconds()}
+    </div>
+  );
 };
