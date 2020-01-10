84bd5dbc0f52120637190c0c653117162fafcc10
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-10 11:41:15 +0100

    Updates

---
+++ precache-manifest.242199760d150161d376ea5fba428bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f796997b94eecc4957e539dbac3a994b",
+    "url": "/color-game/index.html"
+  },
+  {
+    "revision": "a39408fc06f84b2b0ff3",
+    "url": "/color-game/static/css/main.249b1ae1.chunk.css"
+  },
+  {
+    "revision": "f10aec4bbfc1ffa54688",
+    "url": "/color-game/static/js/2.92acca29.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/color-game/static/js/2.92acca29.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a39408fc06f84b2b0ff3",
+    "url": "/color-game/static/js/main.9e013209.chunk.js"
+  },
+  {
+    "revision": "3bce8b37cb11dca07063",
+    "url": "/color-game/static/js/runtime-main.0d3fb743.js"
+  }
+]);
