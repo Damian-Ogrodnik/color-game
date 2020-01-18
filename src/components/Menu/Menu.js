1b1acbd68643ce495499fbbc25c8865620f8a10b
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-18 16:19:24 +0100

    Refactor error handle in Menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -12,23 +12,17 @@ export const Menu = () => {
     state: { startError }
   } = useContext(store);
 
-  const renderError = () => {
-    return (
-      startError && (
-        <Error
-          msg={"You have to select level of difficulty before start."}
-          class_Name={"menu__error"}
-        />
-      )
-    );
-  };
-
   return (
     <div className="menu">
       <div className="menu__wrapper">
         <Header classTitle={"menu"} title={"Color Game"} />
         <Levels />
-        {renderError()}
+        {startError && (
+          <Error
+            msg={"You have to select level of difficulty before start."}
+            class_Name={"menu__error"}
+          />
+        )}
         <Start />
       </div>
       <Help />
