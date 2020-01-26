3337267a7d3e9992e186f8b586a88a5f5b5b1175
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-26 13:56:29 +0100

    ADD react-spinners

--- src/axios/axiosSettings.js
+++ src/axios/axiosSettings.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = `https://color-game-dogrodnik.herokuapp.com/`;
+const URL = `http://color-game-dogrodnik.herokuapp.com/`;
 
 export default axios.create({
   baseURL: URL
