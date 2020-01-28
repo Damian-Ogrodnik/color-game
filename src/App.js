028d8ac3da160fa9cf5446936cdc1af07de989b1
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-28 14:40:49 +0100

    Create footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useContext } from "react";
 import "./css/main.css";
+
 import { store } from "./store";
+
 import { Menu } from "./components/Menu";
 import { Game } from "./components/Game/Game";
+import { Footer } from "./components/Footer";
 
 const App = () => {
   const {
     state: { gameStarted }
   } = useContext(store);
 
-  return <>{gameStarted ? <Game /> : <Menu />}</>;
+  return (
+    <>
+      {gameStarted ? <Game /> : <Menu />}
+      <Footer />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__wrapper">
+        <a
+          href="https://github.com/dogrodnik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          © 2020 DOGRODNIK
+        </a>
+      </div>
+    </div>
+  );
+};
