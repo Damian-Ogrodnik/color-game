1617a3a4069a8f27ae685c0925b8e91f556cadd7
dogrodnik <d.ogrodnik96@gmail.com>
2020-01-30 11:12:48 +0100

    Updates

---
+++ precache-manifest.688a957d39896299e9d9b9eb17a2f076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa61e26197349bb941626a810f4b230d",
+    "url": "/color-game/index.html"
+  },
+  {
+    "revision": "f084eb110d7d72341a82",
+    "url": "/color-game/static/css/main.6a606516.chunk.css"
+  },
+  {
+    "revision": "78fdd1e03db8ce71baec",
+    "url": "/color-game/static/js/2.290a3bc9.chunk.js"
+  },
+  {
+    "revision": "5c03a40424a9b05ced811755741cb16e",
+    "url": "/color-game/static/js/2.290a3bc9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f084eb110d7d72341a82",
+    "url": "/color-game/static/js/main.78bfe882.chunk.js"
+  },
+  {
+    "revision": "3bce8b37cb11dca07063",
+    "url": "/color-game/static/js/runtime-main.0d3fb743.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/color-game/precache-manifest.3a80fea22b2a514799cfba56d78e0b08.js"
+  "/color-game/precache-manifest.688a957d39896299e9d9b9eb17a2f076.js"
 );
 
 self.addEventListener('message', (event) => {
